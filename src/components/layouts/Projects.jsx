a000676767e5ae56548c8e1025a3297aad9dac19
Montajur Rahman <montajurrdev@gmail.com>
2026-02-13 05:10:19 +0600

    project commit

--- src/components/layouts/Projects.jsx
+++ src/components/layouts/Projects.jsx
@@ -1,35 +1,71 @@
 import React from "react";
 import Container from "../Container";
 import Heading from "../Heading";
-import Image from "../Image";
 import project_01 from "/src/assets/project-01.png";
 import project_02 from "/src/assets/project-02.png";
 import project_03 from "/src/assets/project-03.png";
-import { Link } from "react-router-dom";
+import project_04 from "/src/assets/project-04.png";
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
-<section id="work">
-      <Container className={"pt-20"} >
-      <div className="pb-8" >
-        <Heading
-          text="Project Gallery"
-          as="h2"
-          className="text-[40px] font-bold text-center "
-        />
-        <p className="text-[18px]  text-center pt-2">A showcase of my recent projects, featuring clean UI, modern frontend development, and responsive user experiences.</p>
-
-      </div>
-      <div className="flex justify-between items-center flex-wrap text-center gap-y-5 -mx-5">
-        <ProjectCard projectImg={project_01} projectType="Frontend Development" projectCategory="E-Commerce" clientName="Practice Project" liveUrl="https://orebi-website-frontend.netlify.app/" codeUrl={"https://github.com/montajurrahmandev/orebi_website_frontend"} className="" />
-        <ProjectCard projectImg={project_02} projectType="Frontend Development" projectCategory="E-Commerce" clientName="Practice Project" liveUrl="https://montajurrahmandev.github.io/furniro_website_frontend/" codeUrl={"https://github.com/montajurrahmandev/furniro_website_frontend"} className="" />
-        <ProjectCard projectImg={project_03} projectType="Frontend Development" projectCategory="E-Commerce" clientName="Practice Project" liveUrl="https://tech-e-commerce-with-react-and-boot.vercel.app/" codeUrl={"https://github.com/montajurrahmandev/tech_e-commerce_with_react_and_bootstrap"} className="" />
-       
-      </div>
-
-    </Container>
-</section>
+    <section id="work">
+      <Container className={"pt-20"}>
+        <div className="pb-8">
+          <Heading
+            text="Project Gallery"
+            as="h2"
+            className="text-[40px] font-bold text-center "
+          />
+          <p className="text-[18px]  text-center pt-2">
+            A showcase of my recent projects, featuring clean UI, modern
+            frontend development, and responsive user experiences.
+          </p>
+        </div>
+        <div className="flex justify-between items-center flex-wrap text-center gap-y-5 -mx-5">
+          <ProjectCard
+            projectImg={project_01}
+            projectType="Frontend Development"
+            projectCategory="E-Commerce"
+            clientName="Practice Project"
+            liveUrl="https://orebi-website-frontend.netlify.app/"
+            codeUrl={"https://github.com/montajurrdev/orebi_website_frontend"}
+            className=""
+          />
+          <ProjectCard
+            projectImg={project_02}
+            projectType="Frontend Development"
+            projectCategory="E-Commerce"
+            clientName="Practice Project"
+            liveUrl="https://montajurrdev.github.io/furniro_website_frontend/"
+            codeUrl={"https://github.com/montajurrdev/furniro_website_frontend"}
+            className=""
+          />
+          <ProjectCard
+            projectImg={project_03}
+            projectType="Frontend Development"
+            projectCategory="E-Commerce"
+            clientName="Practice Project"
+            liveUrl="https://tech-e-commerce-with-react-and-boot.vercel.app/"
+            codeUrl={
+              "https://github.com/montajurrdev/tech_e-commerce_with_react_and_bootstrap"
+            }
+            className=""
+          />
+          <ProjectCard
+            projectImg={project_04}
+            projectType="Frontend Development"
+            projectCategory="Creative Agency"
+            clientName="Practice Project"
+            liveUrl="https://montajurrdev.github.io/creative-agency__with__vanilla_html-css-js__and_sass/"
+            codeUrl={
+              "https://github.com/montajurrdev/creative-agency__with__vanilla_html-css-js__and_sass"
+            }
+            className=""
+          />
+        </div>
+      </Container>
+    </section>
   );
 };
 
